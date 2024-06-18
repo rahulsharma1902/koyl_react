2d77fa2945e9216d4d4d77db7d0713cb06272c8f
rahulsharma1902 <rahulanku1902@gmail.com>
2024-06-18 11:16:16 +0530

    admin-dashbord-merge

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import LoginPage from './pages/LoginPage';
+import AdminRoutes from './routes/AdminRoutes';
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
             <Router>
                 <Routes>
                     <Route path="/login" element={<LoginPage />} />
+                    <Route path="/*" element={<AdminRoutes />} />
                 </Routes>
             </Router>
         </AuthProvider>
---
+++ src/components/Admin/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const AdminDashboard = () => {
+    return (
+        <div>
+            <div>
+                <h1>Welcome to Admin Dashboard</h1>
+            </div>
+        </div>
+    )
+}
+
+export default AdminDashboard;
